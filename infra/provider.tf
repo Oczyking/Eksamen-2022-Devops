836f10bf6ce2376fce04fba128af865de8ddad11
Oczyking <mat-loe@outlook.com>
2022-12-01 12:26:20 +0000

    Added in backend for s3-bucket and commentated out databucker.tf

--- infra/provider.tf
+++ infra/provider.tf
@@ -5,5 +5,9 @@ terraform {
       version = "4.40.0"
     }
   }
-
+  backend "s3" {
+    bucket = "pgr301-2022-eksamen-state"
+    key    = "1042/eksamen-actions.state"
+    region = "eu-north-1"
+  }
 }
