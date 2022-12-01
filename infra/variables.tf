5073bb3aba990d4b18215f85b7442d137ccedcf4
Oczyking <mat-loe@outlook.com>
2022-12-01 15:59:10 +0000

    Fixed variables gor github actions

--- infra/variables.tf
+++ infra/variables.tf
@@ -1,9 +1,9 @@
 variable "candidate_id" {
   type = string
-  defaults = "1042"
+  default = "1042"
 }
 
 variable "candidate_email"  {
   type = string
-  defaults = "mat-loe@outlook.com"
+  default = "mat-loe@outlook.com"
 }
