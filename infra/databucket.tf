fa5672b8eb55d5801a1b558fbb44d1d91790c140
Oczyking <mat-loe@outlook.com>
2022-12-10 15:16:41 +0000

    Tring to fix teraform and s3 bucket

--- infra/databucket.tf
+++ infra/databucket.tf
@@ -1,4 +1,13 @@
 # Jim; this just fails ... commented it out ! We need to figure this out later, starting new task instead...
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "4.40.0"
+  }
+}
+}
+
 resource "aws_s3_bucket" "analyticsbucket" {
   bucket = "analytics-${var.candidate_id}"
 }
