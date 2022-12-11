0d0e315a72d64dafeb821ebaa9098c2d62966df2
Oczyking <mat-loe@outlook.com>
2022-12-11 19:19:39 +0000

    Alarm

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,13 @@
+resource "aws_sns_topic" "alarms" {
+  name = "alarm-topic-${var.candidate_id}"
+}
+
+resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
+  topic_arn = aws_sns_topic.alarms.arn
+  protocol  = "email"
+  endpoint  = var.candidate_email
+}
+
 resource "aws_cloudwatch_metric_alarm" "zerosum" {
   alarm_name                = "Carts-amount"
   namespace                 = "1042"
@@ -13,14 +23,4 @@ resource "aws_cloudwatch_metric_alarm" "zerosum" {
   alarm_description         = "This alarm goes off as soon as the total amount of carts exceeds more than 5 "
   insufficient_data_actions = []
   alarm_actions       = [aws_sns_topic.alarms.arn]
-}
-
-resource "aws_sns_topic" "alarms" {
-  name = "alarm-topic-${var.candidate_id}"
-}
-
-resource "aws_sns_topic_subscription" "user_updates_sqs_target" {
-  topic_arn = aws_sns_topic.alarms.arn
-  protocol  = "email"
-  endpoint  = var.candidate_email
 }
