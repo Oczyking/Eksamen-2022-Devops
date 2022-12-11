47da1227b10a81794fc546cb8eda6bd2a71ffb79
Oczyking <mat-loe@outlook.com>
2022-12-11 12:28:57 +0000

    Added in alarm

--- infra/sns_topic_for_alarm.tf
+++ infra/sns_topic_for_alarm.tf
@@ -1,3 +1,20 @@
+resource "aws_cloudwatch_metric_alarm" "zerosum" {
+  alarm_name                = "Carts-amount"
+  namespace                 = "1042"
+  metric_name               = "carts.value"
+
+  comparison_operator       = "GreaterThanThreshold"
+  threshold                 = "0"
+  evaluation_periods        = "3"
+  period                    = "300"
+
+  statistic                 = "Maximum"
+
+  alarm_description         = "This alarm goes off as soon as the total amount of carts exceeds more than 5 "
+  insufficient_data_actions = []
+  alarm_actions       = [aws_sns_topic.alarms.arn]
+}
+
 resource "aws_sns_topic" "alarms" {
   name = "alarm-topic-${var.candidate_id}"
 }
