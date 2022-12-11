6685059963f53771f902d6d70e27c49e8cfc5b47
Oczyking <mat-loe@outlook.com>
2022-12-11 19:42:21 +0000

    Tring to add terraform to be ok

--- infra/provider.tf
+++ infra/provider.tf
@@ -11,3 +11,9 @@ terraform {
     region = "eu-west-1"
   }
 }
+
+provider "aws" {
+  access_key = ${{ secrets.AWS_ACCESS_KEY_ID }}
+  secret_key = ${{ secrets.AWS_SECRET_ACCESS_KEY }}
+  region     = "eu-west-1"
+}
